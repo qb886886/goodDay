a5ba9c167f32506cdbab97951bcf697118b8e186
qibiaoWang <542672187@qq.com>
2021-06-17 10:21:39 +0800

    docs: 新增toast文档说明

--- docs/使用vue实现一个toast功能.vue
+++ docs/使用vue实现一个toast功能.vue
@@ -1,6 +1,6 @@
 # 文档地址
 
-![链接](https://www.yuque.com/docs/share/1b56dd08-5cb6-4aa4-9f81-57493c21df05?#（密码：lkxr） 《使用vue实现一个toast功能》)
+[链接](https://www.yuque.com/docs/share/1b56dd08-5cb6-4aa4-9f81-57493c21df05?#（密码：lkxr） 《使用vue实现一个toast功能》)
 
 
 # 组件位置
